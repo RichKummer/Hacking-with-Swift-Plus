0b1af7d27c7466182bac1b5e3ab7c6a2c2e6163f
RichKummer <rckummer@gmail.com>
2020-08-23 16:31:24 -0400

    Bezier Shape

---
+++ BezierPath-Example/BezierPath-Example/BezierPath_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BezierPath_ExampleApp.swift
+//  BezierPath-Example
+//
+//  Created by RichK on 8/23/20.
+//
+
+import SwiftUI
+
+@main
+struct BezierPath_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
