de6f2c2dc6c4198def1a9e74fbc08b7b18aaf9f7
RichKummer <rckummer@gmail.com>
2022-01-23 12:53:39 -0500

    Custom ButtonStyle

---
+++ Intermediate SwiftUI/Basic ButtonStyle/Basic ButtonStyle/Basic_ButtonStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_ButtonStyleApp.swift
+//  Basic ButtonStyle
+//
+//  Created by RichK on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Basic_ButtonStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
